ffaa7efca613bed35e24e2ee082151aa60dcd793
VALBER TAVARES <valbertavares11@gmail.com>
2023-10-03 21:39:52 -0400

    criando SplashScreen

---
+++ app/src/main/java/com/example/conexaocursos/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.conexaocursos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    static int TEMPO_SPLASH_SCREEN = 2000; //ms
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                irParaProximoActivity();
+            }
+
+            private void irParaProximoActivity() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish(); //finaliza o activity
+            }
+        }, TEMPO_SPLASH_SCREEN);
+    }
+}
