ce4cf83f7a7a5dc0cdee0ce251434d392f7f6477
VALBER TAVARES <valbertavares11@gmail.com>
2023-10-03 23:39:14 -0400

    modificando splash screen e alterando a tela de login

--- app/src/main/java/com/example/conexaocursos/SplashScreen.java
+++ app/src/main/java/com/example/conexaocursos/SplashScreen.java
@@ -22,7 +22,7 @@ public class SplashScreen extends AppCompatActivity {
             }
 
             private void irParaProximoActivity() {
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                Intent intent = new Intent(SplashScreen.this, login.class);
                 startActivity(intent);
                 finish(); //finaliza o activity
             }
