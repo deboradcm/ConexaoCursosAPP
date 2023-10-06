95bc8c48c1e988ac1bec4246d90deca132c53fa4
VALBER TAVARES <valbertavares11@gmail.com>
2023-10-06 00:00:56 -0400

    ajuste no layout para alinhar o recycleView na tela da home do aluno

--- app/src/main/java/com/example/conexaocursos/CursoDAO.java
+++ app/src/main/java/com/example/conexaocursos/CursoDAO.java
@@ -14,10 +14,10 @@ public class CursoDAO {
 
     public ArrayList<Curso> getList(){
         if(cursoList.size() == 0) {
-            cursoList.add(new Curso(0, "Introducao a Python", "R$120,00"));
-            cursoList.add(new Curso(1, "Introducao a C", "R$160,00"));
-            cursoList.add(new Curso(2, "Introducao a JAVA", "R$260,00"));
-            cursoList.add(new Curso(3, "Introducao a KOTLIN", "R$200,00"));
+            cursoList.add(new Curso(0, "Introdução ao Python", "R$120,00"));
+            cursoList.add(new Curso(1, "Introdução ao C", "R$160,00"));
+            cursoList.add(new Curso(2, "Introdução ao JAVA", "R$260,00"));
+            //cursoList.add(new Curso(3, "Introducao a KOTLIN", "R$200,00"));
         }
         return cursoList;
     }
